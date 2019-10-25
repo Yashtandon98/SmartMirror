19d3f32f80ee4cdb292ab2bd3b115526e04c83e8
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2019-10-25 17:08:04 +0530

    Added Face recognition Module

---
+++ Modules/facerecog/sr.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+import pyaudio
+
+def speech():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Speak up : ')
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio, language="en-US")
+            print(text)
+        except sr.UnknownValueError:
+            print('Sorry could not recognize your voice')
+
+        except sr.RequestError as e:
+            print("request error")
