b17806855691151c5cdf4044432cf79e7ac1ae74
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2019-11-13 20:18:59 +0530

    Added flask server file and Connecting to weather module

---
+++ App/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from Modules.Weather.weather import query_api
+from pprint import pprint as pp
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def webprint():
+    data = []
+    error = None
+    select = 'Chennai'
+    resp = query_api(select)
+    pp(resp)
+    if resp:
+        data.append(resp)
+        if len(data) != 2:
+            error = 'Bad Response from Weather API'
+            return render_template('home.html', data=data, error=error)
+
+if __name__ == '__main__':
+    app.run()
