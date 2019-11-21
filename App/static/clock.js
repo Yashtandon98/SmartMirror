974d56c09cd0526eb061133113c27c8e2c89eea1
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2019-11-21 19:28:02 +0530

    Structured the code for the webpage and added clock.

---
+++ App/static/clock.js
@@ -0,0 +1,20 @@
+function rTime() {
+
+    var lclock = new Date();
+    var h = lclock.getHours();
+    var m = lclock.getMinutes();
+    var s = lclock.getSeconds();
+
+    var ap = (h < 12) ? 'AM' : 'PM';
+
+    h = (h > 12) ? h - 12 : h;
+
+    h = ('0' + h).slice(-2);
+    m = ('0' + m).slice(-2);
+    s = ('0' + s).slice(-2);
+
+    document.getElementById('clock').innerHTML =
+    '<h1>' + h + ':' + m + ':' + s + ' ' + ap + '</h1>';
+
+    var t = setTimeout(lclock, 500);
+}
