9fbaf58e7ed7e26cde4ee4eec90cffff6f36a903
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2020-01-11 01:08:02 +0530

    Connected the news module and modified the CSS file.

--- App/server.py
+++ App/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from .Modules.Weather.weather import query_api
+from .Modules.News.news import getnews
 from pprint import pprint as pp
 
 app = Flask(__name__)
@@ -7,12 +8,15 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET', 'POST'])
 def webprint():
     data = []
+    ndata = []
     error = None
     select = 'Chennai'
     resp = query_api(select)
+    nresp = getnews()
     pp(resp)
-    if resp:
+    if resp or nresp:
         data.append(resp)
+        ndata.append(nresp)
         if len(data) != 2:
             error = 'Bad Response from Weather API'
-            return render_template('home.html', data=data, error=error)
+            return render_template('home.html', data=data, ndata = ndata, error=error)
