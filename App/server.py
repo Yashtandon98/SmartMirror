c47508804bd463be8fd669a4837c82b1d04529e0
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2020-01-27 12:39:11 +0530

    Added new routes to flask app.

--- App/server.py
+++ App/server.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from .Modules.Weather.weather import query_api
 from pprint import pprint as pp
 from .Modules.News.news import getnews
+from .Modules.Speak.speaker import sdata, wikidata
+
 
 app = Flask(__name__)
 
@@ -13,6 +15,28 @@ def newsprint():
     if nresp:
         ndata.append(nresp)
         return render_template('news.html', ndata = ndata, error = error)
+
+@app.route('/weather', methods = ['GET', 'POST'])
+def weatherprint():
+    data = []
+    error = None
+    select = 'Chennai'
+    resp = query_api(select)
+    if resp:
+        data.append(resp)
+        return render_template('weather.html', data = data, error = error)
+
+@app.route('/wiki', methods = ['GET', 'POST'])
+def wikiprint():
+    wdata = wikidata
+    error = None
+    return render_template('news.html', wdata = wdata, error = error)
+
+@app.route('/sports', methods = ['GET', 'POST'])
+def sportsprint():
+    ndata = sdata
+    error = None
+    return render_template('news.html', ndata = ndata, error = error)
 
 @app.route('/', methods = ['GET', 'POST'])
 def webprint():
