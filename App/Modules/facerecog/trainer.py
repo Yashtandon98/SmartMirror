cfc2481156395cc830f687210bdee50713481c8d
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2020-02-22 17:38:02 +0530

    Added new code to train data.

--- App/Modules/facerecog/trainer.py
+++ App/Modules/facerecog/trainer.py
@@ -5,9 +5,9 @@ from PIL import Image
 
 def train():
     recognizer = cv2.face.LBPHFaceRecognizer_create()
-    path = 'dataset'
-    if not os.path.exists('./recognizer'):
-        os.makedirs('./recognizer')
+    path = 'C:/Users/Yash Tandon/Desktop/python course/SmartMirror/SmartMirror/App/Modules/facerecog/dataset'
+    if not os.path.exists('C:/Users/Yash Tandon/Desktop/python course/SmartMirror/SmartMirror/App/Modules/facerecog/recognizer'):
+        os.makedirs('C:/Users/Yash Tandon/Desktop/python course/SmartMirror/SmartMirror/App/Modules/facerecog/recognizer')
 
     def getImagesWithID(path):
         imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
@@ -19,11 +19,11 @@ def train():
             ID = int(os.path.split(imagePath)[-1].split('.')[1])
             faces.append(faceNp)
             IDs.append(ID)
-            cv2.imshow("training",faceNp)
+            #cv2.imshow("training",faceNp)
             cv2.waitKey(10)
         return np.array(IDs), faces
 
     Ids, faces = getImagesWithID(path)
     recognizer.train(faces,Ids)
-    recognizer.save('recognizer/trainingData.yml')
+    recognizer.save('C:/Users/Yash Tandon/Desktop/python course/SmartMirror/SmartMirror/App/Modules/facerecog/recognizer/trainingData.yml')
     cv2.destroyAllWindows()
